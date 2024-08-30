4f181bff704da689f56e43315d891f6304986b3d
Jed <kohjedrek@yahoo.com.sg>
2024-08-30 14:51:39 +0800

    Updated taskbar and home page

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,12 +1,26 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
-const _layout = () => {
+const AuthLayout = () => {
+  //Stack: A component for managing stack-based navigation between screens in an app. ( think of the Stack component as a controller that handles navigation actions and transitions between different screens.)
+  //Stack.Screen: Defines individual screens within a stack navigator, mapping routes to components.
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen
+          name="sign-in"
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+        <Stack.Screen
+          name="sign-up"
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+      </Stack>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
   );
 };
 
-export default _layout;
+export default AuthLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,49 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
-
+import { Text, View, ScrollView, Image } from "react-native";
+import { router, Redirect } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import HomePageButton from "../components/HomepageButton";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-teal-300 text-5xl font-pblack">
-        Edit app/index.tsx to edit this freaking screen.
-      </Text>
-      <Link href="/profile" style={{ color: "blue" }}>
-        {" "}
-        Go to profile
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full min-h-[85vh] justify-center items-center px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[65px] h-[16px] absolute -bottom-2 right-36"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless
+            expolration with Aora
+          </Text>
+          <HomePageButton
+            title="Continue with Email"
+            handlePress={() => {
+              router.push("/sign-in");
+            }}
+            containerStyles={"w-full mt-7"}
+          />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
